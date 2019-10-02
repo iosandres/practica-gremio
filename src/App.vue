d80a03974a771dfa10ce27c8d712708fba6977c6
Andres <ingmontes21@gmail.com>
2019-10-01 23:04:14 -0500

    initial configuariton to practice vue

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,63 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">Merflix</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Busca tu serie favorita</b-button>
+        </b-nav-form>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+   <b-card-group deck>
+    <b-card title="Title" img-src="https://wallpapersite.com/images/pages/pic_w/15948.jpg" img-alt="Image" img-top to="/detail">
+      <b-card-text>
+        This is a wider card with supporting text below as a natural lead-in to additional content.
+        This content is a little bit longer.
+      </b-card-text>
+    </b-card>
+
+    <b-card title="Title" img-src="https://wallpapersite.com/images/pages/pic_w/15927.jpg" img-alt="Image" img-top>
+      <b-card-text>
+        This card has supporting text below as a natural lead-in to additional content.
+      </b-card-text>
+    </b-card>
+
+    <b-card title="Title" img-src="https://wallpapercave.com/wp/wp1808933.jpg" img-alt="Image" img-top>
+      <b-card-text>
+        This is a wider card with supporting text below as a natural lead-in to additional content.
+        This card has even longer content than the first to show that equal height action.
+      </b-card-text>
+    </b-card>
+  </b-card-group>
+   <b-card-group deck>
+    <b-card title="Title" img-src="https://wallpapersite.com/images/pages/pic_w/17994.jpg" img-alt="Image" img-top>
+      <b-card-text>
+        This is a wider card with supporting text below as a natural lead-in to additional content.
+        This content is a little bit longer.
+      </b-card-text>
+    </b-card>
+
+    <b-card title="Title" img-src="https://wallpapersite.com/images/pages/pic_w/18223.jpg" img-alt="Image" img-top>
+      <b-card-text>
+        This card has supporting text below as a natural lead-in to additional content.
+      </b-card-text>
+    </b-card>
+
+    <b-card title="Title" img-src="https://wallpapersite.com/images/pages/pic_w/17618.jpg" img-alt="Image" img-top>
+      <b-card-text>
+        This is a wider card with supporting text below as a natural lead-in to additional content.
+        This card has even longer content than the first to show that equal height action.
+      </b-card-text>
+    </b-card>
+  </b-card-group>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
